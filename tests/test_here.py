dd1ce9694b3a2059a5de465ceed27363d2502c25
Josh P. Sawyer <josh.p.sawyer@gmail.com>
2020-02-23 10:37:58 -0500

    set_here test complete, find_root recursion added, find_root parameterized

--- tests/test_here.py
+++ tests/test_here.py
@@ -1,34 +1,63 @@
 # -*- coding: utf-8 -*-
 
+import pytest
+
 from pyhere import __version__
 from pyhere import here
+from pyhere import set_here
 from pyhere import find_root
+from pyhere import root_indicators
 
+# this test is to ensure I don't mess up versioning
 def test_version():
-    assert __version__ == "0.1.0"
-    
-def test_touch():
+    assert __version__ == "0.2.0"
+       
+def test_here(tmp_path):
+    # create dummy project
+    # change working directory to src folder
+    # generate here to data in data folder
+    # assert paths
     assert True
     
-def test_here():
-    assert True
+def test_set_here(tmp_path):
+    # create dummy project
+    f1 = tmp_path / "proj" / "proj_dir"
+    f1.mkdir(parents=True, exist_ok=True)
+
+    # create .here in tmp_path
+    set_here()
+    cwd_path = tmp_path.cwd()
+    assert cwd_path.joinpath(".here").exists()
     
-def test_set_here():
-    assert True
+    # create .here in /proj/proj_dir
+    set_here(f1)
+    assert f1.joinpath(".here").exists()
     
-def test_find_root(tmp_path):
+    # create .here in /proj/
+    set_here(str(tmp_path / "proj"))
+    assert tmp_path.joinpath("proj").joinpath(".here").exists()
+    
+@pytest.mark.parametrize(
+    "r_indicator",
+    root_indicators,
+)
+def test_find_root_from_indicator(tmp_path, r_indicator):
     r_indicator = ".here"
     
     # create /proj/r_indicator
-    f1 = tmp_path / "proj"
+    f1 = tmp_path / "proj" / r_indicator
     f1.mkdir(parents=True, exist_ok=True)
 
-    f2 = f1 / "src"
+    # create a different directory, /proj/src/test.txt
+    f2 = f1 / "src" / "test.txt"
     f2.mkdir(parents=True, exist_ok=True)
-    f2 = f2 / "test"
     
+    # verify they point to the same project root
     assert find_root(f1).resolve() == find_root(f2).resolve()
 
-    assert True
-    
-    
+def test_find_sys_root(tmp_path):
+    # create some nested directories
+    f1 = tmp_path / "proj" / "another_path"
+
+    # verify that they all recurse to the system root
+    assert find_root(tmp_path).resolve() == find_root(f1).resolve()
